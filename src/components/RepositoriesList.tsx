f0c4f08c5393efcdbd47af1abec528cef49e31b2
Sergio Cano <canosergio90@gmail.com>
2021-01-26 20:31:31 -0300

    redux useSelector typed and final changes

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { useActions } from '../hooks/useActions'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('')
     const { searchRepositories } = useActions()
+    const { data, error, loading } = useTypedSelector((state) => state.repositories)
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
@@ -17,6 +19,10 @@ const RepositoriesList: React.FC = () => {
                 <input value={term} onChange={e => setTerm(e.target.value)} />
                 <button>Search</button> 
             </form>
+            {error && <h3>{error}</h3>}
+            {loading && <h3>Loading...</h3>}
+            {!error && !loading &&
+                data.map((name) => <div key={name}>{name}</div>)}
         </div>
     )
 }
